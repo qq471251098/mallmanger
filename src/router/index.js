f1b502f496676c598788e2d1bae3b95514940398
qq471251098 <471251098@qq.com>
2019-06-24 11:32:43 +0800

    '新建登录分支，并配置路由'

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {path: '/', redirect: {name: 'Login'}},
+    {name: 'Login', path: '/login', component: Login}
+  ]
 })
