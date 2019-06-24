b09b86390c5693baf852364cc9303cd8e548583c
qq471251098 <471251098@qq.com>
2019-06-24 12:53:17 +0800

    '引入提示框消息'

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>登录</div>
+<div class="login-wrap">
+  <el-form label-position="top" label-width="80px" :model="fromDate" class="login-form">
+    <h2>用户登录</h2>
+    <el-form-item label="用户名">
+      <el-input v-model="fromDate.username"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="fromDate.password"></el-input>
+    </el-form-item>
+    <el-button type="primary" class="login-btn" @click='handleLogin()'>登录</el-button>
+  </el-form>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      fromDate: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post('login',this.fromDate).then( (res) => {
+        const {
+          data,
+          meta: {msg, status}
+        } = res.data
+
+        if (status === 200) {
+          this.$message.success(msg)
+        } else {
+          this.$message.warning(msg)
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style>
+  .login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-wrap .login-form {
+    padding: 30px;
+    width: 400px;
+    background-color: #fff;
+    border-radius: 10px;
+  }
+
+  .login-wrap .login-btn {
+    width: 100%;
+  }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import MyServerHttp from './plugins/http'
 import 'element-ui/lib/theme-chalk/index.css'
+import '../src/assets/css/reset.css'
 import App from './App'
 import router from './router'
 
 Vue.use(ElementUI)
+Vue.use(MyServerHttp)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
