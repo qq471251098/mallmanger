40cfe1cc3c5fb81177dd8cf4357672a0e4acccf3
qq471251098 <471251098@qq.com>
2019-06-24 14:42:59 +0800

    '引入导航组件，调整样式'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -14,7 +14,88 @@
     </el-row>
   </el-header>
   <el-container>
-    <el-aside width="200px" class="aside">Aside</el-aside>
+    <el-aside width="200px" class="aside">
+
+      <el-menu :unique-opened='true'>
+        <!-- 1 -->
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>用户管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>用户列表</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <!-- 2 -->
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>权限管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>角色列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              <span>权限列表</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <!-- 3 -->
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>商品管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>商品列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              <span>分类参数</span>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+              <i class="el-icon-menu"></i>
+              <span>商品分类</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <!-- 4 -->
+        <el-submenu index="4">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>订单管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>订单列表</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <!-- 5 -->
+        <el-submenu index="5">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>数据统计</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">
+              <i class="el-icon-menu"></i>
+              <span>数据统计</span>
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
     <el-main class="main">Main</el-main>
   </el-container>
 </el-container>
